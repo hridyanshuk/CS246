0a271f8050f7416c414b7e65eee10b58b26f55ce
hridyanshuk <hridyanshukumar@gmail.com>
2022-04-06 19:27:54 +0530

    BTrees Folder

---
+++ BTrees/btree.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class dataType
+{
+
+};
+
+class Node
+{
+    int m;
+    linkedList list;
+    public:
+        Node(int m)
+        {
+            this->m=m;
+        }
+};
